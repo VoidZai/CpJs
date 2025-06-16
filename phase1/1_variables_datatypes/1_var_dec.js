7d9f51fc874b87f051d870eb9a571e077f4cf690
Shaikhzai <shaikhston1123@gmail.com>
2025-06-16 10:42:49 +0530

    variable declaration

---
+++ phase1/1_variables_datatypes/1_var_dec.js
@@ -0,0 +1,6 @@
+//old way(avoid in modern js)
+var x= 19; //function scoped
+
+//modern ways(es6+)
+let y= 20; //block scoped, mutable(changeable/modifiable)
+const z= 22; //bloxk scoped, immutable(unchangeable/unmodifiable)
