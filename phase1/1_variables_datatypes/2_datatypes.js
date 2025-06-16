4164024ad0b7a75fd785c7dde66992a9271de046
Shaikhzai <shaikhston1123@gmail.com>
2025-06-16 11:05:58 +0530

    7 premetive datatypes

---
+++ phase1/1_variables_datatypes/2_datatypes.js
@@ -0,0 +1,24 @@
+// premetive datatypes are immutble and single values which are diretly stored in mrmory and do not hve methods.
+// thre are 7 premetive datatypes in js
+
+//1) Number: both interger and floating-points
+const score= 100;
+const precesion= 3.14159;
+
+//2) BigInt: for very large integers(es2020)
+const bigInt= 9007199254740991n;
+
+//3) String: sequence of characters/text data
+const name= "John";
+
+//4) Boolean: true or false
+const isActive= true;
+
+//5) Null: a single null value/intentional absence of value
+const car = null;
+
+//6) Undefined: a variable that has not been assigned a value yet
+let age;
+
+//7) Symbol: a unique and immutable primitive value/unique identifier (es6)
+const id = Symbol('unique');
